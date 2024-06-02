c2f71554f0ad726f9ab2cfadac079713d0771b9b
Snak <snak.hoshinonaka@gmail.com>
2024-06-02 23:04:17 +0900

    vanilla-extractを使う

--- src/components/pages/bureaus/index.tsx
+++ src/components/pages/bureaus/index.tsx
@@ -1,11 +1,13 @@
 import { bureausAtom } from "@/store/bureaus"
 import { useAtomValue } from "jotai"
+import { bureausStyle } from "./style.css"
 
 export const BureausPageComponent = () => {
   const bureaus = useAtomValue(bureausAtom)
 
   return (
     <>
+      <header className="green">ほしのなか政府</header>
       {bureaus.map((bureau, i) => (
         <div key={i}>{bureau.name}</div>
       ))}
---
+++ src/components/pages/bureaus/style.css.ts
@@ -0,0 +1,7 @@
+import { style } from "@vanilla-extract/css"
+
+export const bureausStyle = {
+  header: style({
+    backgroundColor: "green",
+  }),
+}
