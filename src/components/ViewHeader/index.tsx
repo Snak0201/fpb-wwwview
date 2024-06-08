953f979842250a34bc6ea6aa4c03b943f45b7230
Snak <snak.hoshinonaka@gmail.com>
2024-06-08 11:03:15 +0900

    背景色をつける

--- src/components/ViewHeader/index.tsx
+++ src/components/ViewHeader/index.tsx
@@ -1,6 +1,14 @@
+import { ViewHeaderStyles } from "./style.css"
+
 /**
  * ヘッダー
  */
 export const ViewHeader = () => {
-  return <>ここがheader</>
+  return (
+    <div className={ViewHeaderStyles.links}>
+      <a href="articles">記事一覧</a>
+      <a href="tools">ツール</a>
+      <a href="about">ほしのなか政府について</a>
+    </div>
+  )
 }
---
+++ src/components/ViewHeader/style.css.ts
@@ -0,0 +1,7 @@
+import { style } from "@vanilla-extract/css"
+
+export const ViewHeaderStyles = {
+  links: style({
+    backgroundColor: "aquamarine",
+  }),
+}
