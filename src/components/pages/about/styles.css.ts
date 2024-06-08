a1a3069a453e1298c7d8c0afed6c1dba62521ea0
Snak <snak.hoshinonaka@gmail.com>
2024-06-08 20:35:34 +0900

    画像の表示を修正する

--- src/components/pages/about/styles.css.ts
+++ src/components/pages/about/styles.css.ts
@@ -1,8 +1,15 @@
+import { mediaQuery } from "@/constants/mediaQuery"
 import { style } from "@vanilla-extract/css"
 
 export const styles = {
   logo: style({
     margin: "auto",
     display: "block",
+    "@media": {
+      [mediaQuery.sp]: {
+        width: "300px",
+        height: "105px",
+      },
+    },
   }),
 }
