0d76352c0ee7799dc5e6142b11684404b5076662
Snak <snak.hoshinonaka@gmail.com>
2024-06-08 12:17:54 +0900

    ヘッダーに画像を表示する

--- src/components/ViewHeader/index.tsx
+++ src/components/ViewHeader/index.tsx
@@ -1,14 +1,31 @@
 import { styles } from "@/components/ViewHeader/style.css"
+import Image from "next/image"
+
+interface NavLink {
+  href: string
+  text: string
+}
 
 /**
  * ヘッダー
  */
 export const ViewHeader = () => {
+  const navLinks: NavLink[] = [
+    { href: "articles", text: "記事一覧" },
+    { href: "tools", text: "ツール" },
+    { href: "about", text: "ほしのなか政府について" },
+  ]
+
   return (
     <div className={styles.links}>
-      <a href="articles">記事一覧</a>
-      <a href="tools">ツール</a>
-      <a href="about">ほしのなか政府について</a>
+      <Image src="/images/logo.png" alt="ロゴ画像" width="200" height="70" />
+      {navLinks.map((link, index) => {
+        return (
+          <a key={index} href={link.href} className={styles.link}>
+            {link.text}
+          </a>
+        )
+      })}
     </div>
   )
 }
--- src/components/ViewHeader/style.css.ts
+++ src/components/ViewHeader/style.css.ts
@@ -2,6 +2,12 @@ import { style } from "@vanilla-extract/css"
 
 export const styles = {
   links: style({
+    height: "70px",
     backgroundColor: "aquamarine",
+  }),
+
+  link: style({
+    margin: "10px",
+    textDecoration: "none",
   }),
 }
