50abdd3346531c3a449fc4dd43f6e76b36e3ffb3
Snak <72hoshino.snak@gmail.com>
2024-06-09 22:46:59 +0900

    記事一覧要素を追加する

--- src/components/pages/articles/index.tsx
+++ src/components/pages/articles/index.tsx
@@ -1,8 +1,20 @@
+import { ViewContainer } from "@/components/ViewContainer"
+import { ViewLayout } from "@/layouts"
 import { articlesBaseAtom } from "@/store/articles"
 import { useAtomValue } from "jotai"
 
 export const ArticlesPageComponent = () => {
   const articlesBase = useAtomValue(articlesBaseAtom)
-  console.log(articlesBase)
-  return <>Articles</>
+
+  return (
+    <ViewLayout title="記事一覧">
+      <ViewContainer>
+        <ul>
+          {articlesBase.nodes?.map((article, index) => {
+            return <li key={index}>{article?.title}</li>
+          })}
+        </ul>
+      </ViewContainer>
+    </ViewLayout>
+  )
 }
