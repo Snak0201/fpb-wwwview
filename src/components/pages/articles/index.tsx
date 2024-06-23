ca02cfa4ea89caf9f34d99aa6f12b7c43e16104d
Snak <snak.hoshinonaka@gmail.com>
2024-06-23 17:55:04 +0900

    記事一覧ページを作成する

---
+++ src/components/pages/articles/index.tsx
@@ -0,0 +1,3 @@
+export const ArticlesPageComponent = () => {
+  return <>ArticlesPageComponent</>
+}
---
+++ src/pages/articles/index.tsx
@@ -0,0 +1,50 @@
+import { ArticlesPageComponent } from "@/components/pages/articles"
+import { GetServerSideProps } from "next"
+import { initUrqlClient, withUrqlClient } from "next-urql"
+import { cacheExchange, fetchExchange, ssrExchange } from "urql"
+
+interface Props {}
+
+const ArticlesPage = () => {
+  // const [bureaus] = useGetBureausQuery()
+  // useHydrateAtoms([[bureausAtom, bureaus.data?.bureaus || emptyBureaus]])
+
+  return <ArticlesPageComponent />
+}
+
+export const getServerSideProps = (async (context) => {
+  const ssrCache = ssrExchange({ isClient: false })
+  const client = initUrqlClient(
+    {
+      url: `${process.env.NEXT_PUBLIC_WWWSITE_CONTAINER_ROOT}graphql`,
+      exchanges: [cacheExchange, ssrCache, fetchExchange],
+    },
+    false
+  )
+
+  // try {
+  //   await Promise.all([client.query(GetBureausDocument, {}).toPromise()])
+  // } catch (e) {
+  //   console.log(e)
+  // }
+
+  if (!ssrCache.extractData()) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+    },
+  }
+}) satisfies GetServerSideProps<Props>
+
+export default withUrqlClient(
+  (ssrExchange) => ({
+    url: `${process.env.NEXT_PUBLIC_WWWSITE_CONTAINER_ROOT}graphql`,
+    exchanges: [cacheExchange, ssrExchange, fetchExchange],
+  }),
+  { ssr: false }
+)(ArticlesPage)
