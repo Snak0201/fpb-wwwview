a051d6a32fe01a77ded85d633f5cc2bed55acc01
Snak <snak.hoshinonaka@gmail.com>
2024-07-13 18:24:07 +0900

    日時のフォーマットを追加する

--- src/components/ArticleListItem/index.tsx
+++ src/components/ArticleListItem/index.tsx
@@ -9,25 +9,37 @@ interface Props {
  * 記事一覧内の一アイテム
  */
 export const ArticleListItem = ({ article }: Props) => {
-  const published = new Date(article.publishedAt)
-
-  const formatter = new Intl.DateTimeFormat("ja-JP", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false,
-  })
-  const formattedDate = formatter.format(published)
+  /**
+   * タイムゾーン付きの日時文字列を変換する
+   *
+   * 2024-06-16T13:36:09+09:00 => 2024年06月16日 13時36分
+   *  */
+  const formattedPublishedAt = (datetimezoneString: string) => {
+    const dateString = datetimezoneString.split("T")[0]
+    const [yearString, monthString, dayString] = dateString.split("-")
+    const [hourString, minuteString] = datetimezoneString
+      .split("T")[1]
+      .split("+")[0]
+      .split(":")
+    return `${yearString}年${monthString}月${dayString}日 ${hourString}時${minuteString}分`
+  }
 
   return (
     <div className={styles.card}>
       <a href={`articles/${article.id}`}>{article.title}</a>
-      {!!formattedDate && <p>{formattedDate}</p>}
-      {article.bureaus?.map((bureau) => (
-        <>{bureau.name}　</>
-      ))}
+
+      {!!article.publishedAt && (
+        <p>公開日時: {formattedPublishedAt(article.publishedAt)}</p>
+      )}
+
+      {!!article.bureaus?.length && (
+        <p>
+          管轄局:{" "}
+          {article.bureaus.map((bureau, index) => (
+            <span key={index}>{bureau.name} </span>
+          ))}
+        </p>
+      )}
     </div>
   )
 }
