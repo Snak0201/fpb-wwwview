1cecb806bd8c0096804347d668746bb1ab541f48
Snak <snak.hoshinonaka@gmail.com>
2024-07-14 13:08:05 +0900

    色を定数化する

--- src/components/ViewButton/style.css.ts
+++ src/components/ViewButton/style.css.ts
@@ -1,3 +1,4 @@
+import { color } from "@/constants/color"
 import { mediaQuery } from "@/constants/mediaQuery"
 import { style } from "@vanilla-extract/css"
 
@@ -9,16 +10,16 @@ export const styles = {
     padding: "1rem 4rem",
     fontWeight: "bold",
     fontSize: "20px",
-    border: "2px solid #4E5BA8",
-    color: "#FFFFFF",
-    backgroundColor: "#4E5BA8",
+    border: `2px solid ${color.blue}`,
+    color: color.white,
+    backgroundColor: color.blue,
     borderRadius: "100vh",
     transition: "0.5s",
     display: "block",
 
     ":hover": {
-      color: "#4E5BA8",
-      backgroundColor: "#FFFFFF",
+      color: color.blue,
+      backgroundColor: color.white,
     },
     "@media": {
       [mediaQuery.tab]: {
@@ -40,15 +41,15 @@ export const styles = {
     padding: "1rem 4rem",
     fontWeight: "bold",
     fontSize: "20px",
-    border: "2px solid #F05A5A",
-    color: "#FFFFFF",
-    backgroundColor: "#F05A5A",
+    border: `2px solid ${color.alertRed}`,
+    color: color.white,
+    backgroundColor: color.alertRed,
     borderRadius: "100vh",
     transition: "0.5s",
     display: "block",
     ":hover": {
-      color: "#F05A5A",
-      backgroundColor: "#FFFFFF",
+      color: color.alertRed,
+      backgroundColor: color.white,
     },
     selectors: {
       "&.holizontal": {
---
+++ src/constants/color.ts
@@ -0,0 +1,10 @@
+export const characterColor = {
+  aoi_ipr: "#4E5BA8",
+  xue_wds: "#F05A5A",
+}
+
+export const color = {
+  blue: characterColor.aoi_ipr,
+  alertRed: characterColor.xue_wds,
+  white: "#FFFFFF",
+}
